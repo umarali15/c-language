d800de5e712454282b4e90e6fd8d5c13f69b04e1
umarali15 <167315972+umarali15@users.noreply.github.com>
2024-05-30 23:32:41 +0500

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+int main()
+{
+	printf("umar  ali\n") ;
+	int x=2;
+	int y=3;
+	int umar=x+y;
+	
+	printf("youR value is:%d",umar);
+
+printf("the sum of two numbers is: %d \n" , umar);
+   int x;
+    int y;
+    int u; // for sum
+    int m; // for difference
+    int a; // for product
+    int r; // for division
+
+    printf("Give the value of x: \n");
+    scanf("%d", &x);
+
+    printf("Give the value of y: \n");
+    scanf("%d", &y);
+
+    u = x + y;
+   printf("The sum of the two numbers is: %d\n", u);*/
+    int x,y,u,m,a;
+    float rf;
+    printf("enter the amount of x: \n");
+    scanf("%d",&x);
+    printf("enter the amount of y: \n");
+    scanf("%d",&y);
+    u=x+y;
+    printf("adding is equal to : %d \n", u);
+    m=x-y;
+   printf("subtriction is equal to: %d \n" ,m);
+    a=x*y;
+    printf("multiplication is equal to: %d \n",a);
+    rf=x/y;
+    printf("division is equal to: %f",rf);
+float f;
+f=3.14;
+printf("the value of f is : %d",f);
+  find the area of square
+  float side;
+  printf("give the value of one side of square: \n");
+  scanf("%f",&side);
+    printf("area is: %f \n",side*side);
+    printf("area is: %f",side+side);
+int number;
+
+printf("enter the number:\n");
+scanf("%d",&number);
+if(number>=10){
+	printf("you are over 50");
+}
+    else{
+    	printf("you are ");
+	}
+	return 0;
+}
