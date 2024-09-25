6409d0d323a5ca4a68662266d1756afea06bca6f
umarali15 <167315972+umarali15@users.noreply.github.com>
2024-09-25 23:22:50 +0500

    Add files via upload

---
+++ volume of cube.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	float radius,height,volume,i=0.333;
+	printf("ENTER THE VALUE OF RADIUS=");
+	scanf("%f",&radius);
+	printf("ENTER THE HEIGHT OF CUBE=");
+	scanf("%f",&height);
+	volume=i*3.14*radius*radius*height;
+	printf("VOLUME=%f",volume);
+	
+	
+	
+	
+	return 0;
+}
