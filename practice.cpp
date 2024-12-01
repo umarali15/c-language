70863ce5eca25f8c474117b171e45bea1cc3c90a
Umar AlI <167315972+umarali15@users.noreply.github.com>
2024-12-01 18:37:47 +0500

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -518,5 +518,148 @@ int main(){
   return 0;
 
 }
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+	printf("------------------------program1--------------------------------------\n");
+//	float f;
+//	printf("enter the float number:");
+//	scanf("%f",&f);
+//	int u=f;
+//	printf("whole part is:%d\n",u);
+//	float y=f-u;
+//	printf("fractional part is:%.4f",y);
+	printf("------------------------program2--------------------------------------\n");
+//	
+//	int a,b,tem=0;
+//	printf("enter the value of a:");
+//	scanf("%d",&a);
+//	printf("enter the  value of b:");
+//	scanf("%d",&b);
+//	
+//	tem=a;
+//	a=b;
+//	b=tem;
+//	printf("after swapping value of a is: %d\n",a);
+//	printf("after swapping value of b is: %d",b);
+		printf("------------------------program3--------------------------------------\n");
+//	int a,b;
+//	printf("enter the value of a:");
+//	scanf("%d",&a);
+//	printf("enter the value of b:");
+//	scanf("%d",&b);
+//	a=a+b;
+//	b=a-b;
+//	a=a-b;
+//	printf("after swapping value of a:%d\n",a);
+//	printf("after swapping value of b:%d\n",b);
+
+	printf("------------------------program4--------------------------------------\n");
+
+//int num;
+//printf("enter the number:");
+//scanf("%d",&num);
+// int u=num%10;
+// num=num/10;
+// int m=num%10;
+// num=num/10;
+// int a=num%10;
+// num=num/10;
+// int r=num%10;
+// num=num/10;
+// int l=num%10;
+// num=num/10;
+// printf("sum of digits is:%d",u+m+a+r+l);
+ 
+printf("------------------------program5--------------------------------------\n");
+
+//int rem,rev=0,num;
+//printf("enter the number:");
+//scanf("%d",&num);
+//while(num!=0){
+//	rem=num%10;
+//	rev=rev*10+rem;
+//	num=num/10;
+//	
+//}
+//printf("reverse of number is:%d",rev);
+
+// another program of reverse a number
+
+//int num;
+//printf("enter the number:");
+//scanf("%d",num);
+//int u=num%10000;
+//num=num/10000;
+//int m=num%1000;
+//num=num/1000;
+//
+//int a=num%100;
+//num=num/100;
+//
+//
+//int r=num%10;
+//num=num/10;
+//int rev=r*10000+a*1000+m*100+u*10;
+//printf("your reverse value is:%d",rev);
+printf("-----------------program of even odd---------------\n");
+//int c;
+//printf("enter the number");
+//scanf("%d",&c);
+//if(c%2==0){
+//	printf("%d is even",c);
+//}
+//else{
+//	printf("%d is odd",c);
+//}
+printf("-----------------vovel and constant---------------\n");
+//char c;
+//printf("enter any character:");
+//scanf("%c",&c);
+//if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+//	printf("you entered the vovel");
+//}
+//else{
+//	printf("you entered the constant");
+//}
+printf("-----------------positive and negtive---------------\n");
+//int x;
+//printf("enter any number:");
+//scanf("%d",&x);
+//if(x>0){
+//	printf("your entered the positive digit");
+//}
+//else{
+//	printf("you entered the negtive digit");
+//}
+printf("-----------------scholarship---------------\n");
+//float marks,nts;
+//printf("your your fsc marks:");
+//scanf("%f",&marks);
+//printf("your your nts marks:");
+//scanf("%f",&nts);
+//if(marks>=80||nts>=80){
+//	printf("you are eligible");
+//}
+//else{
+//	printf("you are not eligible");
+//}
+////////////////////////////////////////////////////
+int num;
+printf("enter the number of missiles whose iran striked on israel:");
+scanf("%d",&num);
+if(num<100){
+	printf("israel can servive");
+}
+else if(num>100)
+{
+	printf("israel can,t survive");
+}
+else{
+	printf("kindly enter any number");
+}
+	return 0;
+}
 
 
